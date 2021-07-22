f40c2bd04c3ec250a236894852c4856d94c1d290
Alex Breno Santos Souza <alexbreno2005@gmail.com>
2021-07-22 20:24:21 -0300

    Projeto Stock-Trader em VueJS

---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1 class="display-3 font-weight-light mb-4">
+            Negocie e Consulte sua Ações
+        </h1>
+        <v-sheet :elevation="6" class="pa-2 primary">
+            <v-icon class="white--text mr-3 font-weight-light">info</v-icon>
+            <span>Você pode Salvar & Carregar os Dados</span>
+        </v-sheet>
+        <v-sheet :elevation="6" class="pa-2 success darken-1 mt-3">
+            <v-icon class="white--text mr-3 font-weight-light">info</v-icon>
+            <span>Clique em 'Finalizar Dia' para iniciar um novo dia!</span>
+        </v-sheet>
+        <v-divider class="my-4"></v-divider>
+        <p class="display-1"><strong>Seu saldo:</strong> {{ funds | currency}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'home',
+    computed: {
+        funds() {
+            return this.$store.getters.funds
+        }
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-layout row wrap>
+        <Stock v-for="stock in stocks" :key="stock.name" :stock="stock" />
+    </v-layout>
+</template>
+
+<script>
+import Stock from './Stock.vue'
+
+export default {
+    name: 'stocks',
+    components: {Stock},
+    computed: {
+        stocks() {
+            return this.$store.getters.stocks
+        }
+    }
+};
+</script>
+
+<style>
+</style>
